c0a704d23e9382010e2b7de8c06568a521f1b5f8
lnhog403 <lnhog403@gmail.com>
2019-09-04 17:30:54 +0900

    game

--- contracts/gamble.sol
+++ contracts/gamble.sol
@@ -1,10 +1,10 @@
-pragma solidity >=0.4.18 <0.6.0;
+pragma solidity >=0.5.1 <0.6.0;
 contract gamble {
     uint public num1;
     uint public num2;
     uint public result;
 
-    function rand() public payable {
+    function game() public payable {
         // require(_player == msg.sender);
         for (uint i = 0; i < 2; i++) {
             uint randNum = uint(keccak256(abi.encodePacked(now, msg.sender, i))) % 6;
@@ -12,9 +12,7 @@ contract gamble {
             if (i == 0) num1 = randNum;
             else num2 = randNum;
         }
-    }
-    
-    function check() public payable {
+        
         if (num1 == num2) result = 1;
         else result = 0;
     }
