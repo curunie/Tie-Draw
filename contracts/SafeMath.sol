c702991da847800fdf86d64a74eeae2c77d7fd5b
forgong <qeqe3939@naver.com>
2019-09-10 10:48:29 +0900

    Dice token document

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,9 +1,6 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
+
 library SafeMath {
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     if (a == 0) {
@@ -15,9 +12,7 @@ library SafeMath {
   }
 
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
