95d1c966f1ccb7d5da3fc0313883a1edcd98012b
corgi kim <dnflwlq3231@naver.com>
2019-09-20 17:53:18 +0900

    frontend

--- src/library.js
+++
@@ -1,6 +0,0 @@
-async function getToken() {
-    const game_contract = Contract;
-    // 위의 game_contract로부터 getTokens 함수를 call 함수를 통해 반환, record 에 저장
-    const record = await game_contract.methods.getTokens().call();
-    console.log(record);
-}
