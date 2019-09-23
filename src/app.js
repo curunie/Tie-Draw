1349aee21552395e4308b63549f6944d5c0923f8
lnhog403 <lnhog403@gmail.com>
2019-09-23 16:25:42 +0900

    ejx에서 복구

--- src/app.js
+++ src/app.js
@@ -1,31 +1,18 @@
 var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var session = require('express-session');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var fs = require('fs');
-
-app.use(express.static("public"));
-
-app.get('/', function (req,res) {
-    res.render('index')
-});
-
-app.set('views', __dirname + '/views');
-app.set("view engine", 'ejs');
-app.engine('html', require('ejs').renderFile);
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({
-    secret: 'sBld-2eSU-w0gj',
-    reserve: false,
-    saveUninitialized: true
-}));
 
 
 var server = app.listen(3000, function () {
     console.log("Server is Running... port 3000");
+})
+
+
+app.use(express.static("public"));
+app.get('/', function (req,res) {
+    res.sendFile(__dirname + "/index.html");
 })
 
 
