1eeed6d57422c49612143fef7f2fd3908de344a4
forgong <qeqe3939@naver.com>
2019-09-26 16:45:34 +0900

    RPS

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -1,36 +1,27 @@
 pragma solidity >=0.5.0 <0.6.0;
 contract RockPaperScissors {
-    uint256 public Result = 5;
+    uint256 public RPSResult = 5;
     uint256 public hand;
 
-    function StartRPS(uint RPS) public {
-        require(1 < RPS && RPS < 4);
-        uint a;
-        uint b;
-        uint c;
+    function RPStart(uint RPS) public {
+        require(0 < RPS && RPS < 4);
         uint256 randNum = uint256(keccak256(abi.encodePacked(now, msg.sender))) % 3;
         randNum = randNum + 1;
-        if(randNum == 1) hand = a;
-        else if(randNum == 2) hand = b;
-        else hand = c;
-
+        
         if(RPS == 1) {
-            if(hand == a || hand == b) Result = 0;
-            else Result = 1;
+            if(hand == 1 || hand == 2) RPSResult = 0;
+            else RPSResult = 1;
         }
 
         if(RPS == 2) {
-            if(hand == b || hand == c) Result = 0;
-            else Result = 1;
+            if(hand == 2 || hand == 3) RPSResult = 0;
+            else RPSResult = 1;
         }
 
         if(RPS == 3) {
-            if(hand == c || hand == a) Result = 0;
-            else Result = 1;
+            if(hand == 3 || hand == 1) RPSResult = 0;
+            else RPSResult = 1;
         }
 
     }
 }
-
-
-/////////////////////////// 보류
