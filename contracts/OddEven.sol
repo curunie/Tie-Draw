30627f7d38f0da51ae7611ccebe989fc99992dea
forgong <qeqe3939@naver.com>
2019-09-26 16:45:06 +0900

    OddEven

--- contracts/OddEven.sol
+++ contracts/OddEven.sol
@@ -1,9 +1,10 @@
 pragma solidity >=0.5.0 <0.6.0;
 contract OddEven {
-    uint256 public Result = 5;
+    uint256 public OddEvenResult = 5;
     uint256 public GameOddEven;
 
     function OddEvenStart(uint i) public {
+        require(i == 0 || i == 1);
         uint256 randNum = uint256(keccak256(abi.encodePacked(now, msg.sender))) % 2;
         randNum = randNum + 1;
         if(randNum % 2 == 0) {
@@ -11,7 +12,7 @@ contract OddEven {
         }
         else GameOddEven = 1;
 
-        if(i == GameOddEven) Result = 1;
-        else Result = 0;
+        if(i == GameOddEven) OddEvenResult = 1;
+        else OddEvenResult = 0;
     }
 }
