94dfc2cd384144315dbb05c38a1888a2d579f90d
unknown <dnflwlq3231@naver.com>
2019-09-29 20:14:01 +0900

    wrapup game contracts

---
+++ contracts/OddEven.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.5.0 <0.6.0;
+contract OddEven {
+    uint256 public OddEvenResult = 5;
+    uint256 public GameOddEven;
+
+    function OddEvenStart(uint i) public {
+        require(i == 0 || i == 1);
+        uint256 randNum = uint256(keccak256(abi.encodePacked(now, msg.sender))) % 2;
+        randNum = randNum + 1;
+        if(randNum % 2 == 0) {
+            GameOddEven = 0;
+        }
+        else GameOddEven = 1;
+
+        if(i == GameOddEven) OddEvenResult = 1;
+        else OddEvenResult = 0;
+    }
+}
---
+++ contracts/RockPaperScissors.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.5.0 <0.6.0;
+contract RockPaperScissors {
+    uint256 public RPSResult = 5;
+    uint256 public hand;
+
+    function RPStart(uint RPS) public {
+        require(0 < RPS && RPS < 4);
+        uint256 randNum = uint256(keccak256(abi.encodePacked(now, msg.sender))) % 3;
+        randNum = randNum + 1;
+        
+        if(RPS == 1) {
+            if(hand == 1 || hand == 2) RPSResult = 0;
+            else RPSResult = 1;
+        }
+
+        if(RPS == 2) {
+            if(hand == 2 || hand == 3) RPSResult = 0;
+            else RPSResult = 1;
+        }
+
+        if(RPS == 3) {
+            if(hand == 3 || hand == 1) RPSResult = 0;
+            else RPSResult = 1;
+        }
+
+    }
+}
